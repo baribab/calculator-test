777aa6fece58456ec3649905149ad754adba1ffc
CHRISTIAN MOREIRA DE BARROS <christian_m_barros@estudante.sesisenai.org.br>
2026-03-16 16:22:28 -0300

    Initial commit

---
+++ src/coreMath.js
@@ -0,0 +1,15 @@
+export function sumFunction(x, y) {
+  return x + y;
+}
+
+export function subFunction(x, y) {
+  return x - y;
+}
+
+export function multFunction(x, y) {
+  return x * y;
+}
+
+export function divFunction(x, y) {
+  return x / y;
+}
