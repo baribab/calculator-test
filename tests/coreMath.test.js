6aa675bad9c687e35ed51f0f38f97373f8690916
CHRISTIAN MOREIRA DE BARROS <christian_m_barros@estudante.sesisenai.org.br>
2026-03-16 16:45:44 -0300

    TESTANDO FUNÇÕES COM VITEST

---
+++ tests/coreMath.test.js
@@ -0,0 +1,54 @@
+import { describe, test, expect } from "vitest";
+import { sumFunction, subFunction, multFunction, divFunction } from "../src/coreMath";
+
+// sum
+
+describe("sum", () => {
+  test("Essa função deve somar dois valores e retornar o resultado", () => {
+    const num1 = 10;
+    const num2 = 10;
+
+    const result = sumFunction(num1, num2);
+
+    expect(result).toBe(20);
+  });
+});
+
+// sub
+
+// describe("sub", () => {
+//   test("Essa função deve subtrair dois valores e retornar o resultado", () => {
+//     const num1 = 10;
+//     const num2 = 20;
+
+//     const result = subFunction(num1, num2);
+
+//     expect(result).toBe(-10);
+//   });
+// });
+
+// // multi
+
+// describe("mult", () => {
+//   test("Essa função deve multiplicar dois valores e retornar o resultado", () => {
+//     const num1 = 1;
+//     const num2 = 20;
+
+//     const result = multFunction(num1, num2);
+
+//     expect(result).toBe(20);
+//   });
+// });
+
+// // div
+
+// describe("div", () => {
+//   test("Essa função deve dividir dois valores e retornar o resultado", () => {
+//     const num1 = 30;
+//     const num2 = 2;
+
+//     const result = divFunction(num1, num2);
+
+//     expect(result).toBe(15);
+//   });
+// });
